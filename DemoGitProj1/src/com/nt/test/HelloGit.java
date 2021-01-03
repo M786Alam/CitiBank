18a0b028896261e1290870703befdad579e29eb8
Dev2 <Dev2@gmail.com>
2021-01-03 11:47:24 +0530

    calling the methode by Dev1

--- DemoGitProj1/src/com/nt/test/HelloGit.java
+++ DemoGitProj1/src/com/nt/test/HelloGit.java
@@ -1,6 +1,14 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class HelloGit {
+	
+	public static void main(String[] args) {
+		Utility utl = new Utility();
+		System.out.println(utl.getSysDate());
+		
+	}
 
 	
 
